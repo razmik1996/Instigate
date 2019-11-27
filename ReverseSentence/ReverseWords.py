e89b3d705c0c893badc05b027d13e7cf843df435
Razmik Martirosyan <razmik.martirosyan@bpcarm.org>
2019-11-27 23:37:11 +0400

    Final modified

--- ReverseSentence/ReverseWords.py
+++ ReverseSentence/ReverseWords.py
@@ -3,32 +3,50 @@
 import sys
 import signal
 
-def main(): 
+def main():
+    """
+    Main function
+    """
+    print "for information please read readme file"
     signal.signal(signal.SIGINT, signal_handler) #handling SIGINT
     wordList = listWithWords()
     wordList = reverseList(wordList)
     wordList = fromListToString(wordList)
     print wordList                              #Print String
 
-#SIGINT handling function
 def signal_handler(sig, frame):
-        print('\nYou pressed Ctrl+C!')
-        print("Bye!!!")
-        sys.exit(0)
+    """
+    SIGINT handling function
+    """
+    print('\nYou pressed Ctrl+C!')
+    print("Bye!!!")
+    sys.exit(0)
 
-#get string and return spitted string by list
 def listWithWords():
-    string = raw_input("Enter your string: ")
+    """
+    Get string and return spitted string by list
+    """
+    try:
+        print"----------------------------------"
+        string = raw_input("Enter your string: " )             #try block for catching ctr+d 
+    except EOFError as error:
+        print "\nYou pressed ctr+d"
+        print "Bye"
+        exit(1)
     lst = string.split(" ")
     return lst
 
-#Get list and reverse it
 def reverseList(listForReverse):
+    """
+    Get list and reverse it
+    """
     listForReverse = listForReverse[::-1] 
     return listForReverse
 
-#get list and return string
 def fromListToString(listForToString):
+    """
+    Get list and return string
+    """
     string = " ".join(listForToString)
     return string
 
