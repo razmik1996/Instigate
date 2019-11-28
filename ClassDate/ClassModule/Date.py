3134701b4f06f4953ea04c4fc45df190aeb5024d
Razmik Martirosyan <razmik.martirosyan@bpcarm.org>
2019-11-28 14:06:36 +0400

    Date class started

---
+++ ClassDate/ClassModule/Date.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+class Date:
+    __day = 1
+    __year = 1990
+    __mounth = 1
+    __mounthList = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def __init__(self, day, year, mounth):
+        setYear(year)
+        setMounth(mounth)
+        setDay(day)
+    
+
+    def setDay(self, day):
+
+    
+    def setYear(self, year):
+        
+
+    def setMounth(self, mounth):
+
+
+    def getDay(self):
+
+
+    def getYear(self):
+
+
+    def getMounth(self):
+
+
+    def addDay(self, day):
+
+
+    def addYear(self, year):
+
+
+    def addMounth(self, mounth):
+
+
+    def printDate(self):
+
+
+    def diff(self, Date):
